aef67bf45f05f4779c1fb2d2cfa949ad6e06749f
Ruhul12 <ruhulaminparvez007@gmail.com>
2021-02-26 13:28:40 +0600

    Day-0, Hello World, Data Type of JavaScript :rocket:

---
+++ HackerRank/Days-0/Hello-World/hello-world.js
@@ -0,0 +1,9 @@
+var parameterVariable = "Welcome to 10 Days of JavaScript";
+
+
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+}
+
+greeting(parameterVariable);

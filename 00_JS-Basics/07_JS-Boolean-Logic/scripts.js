cd8716ff2d5a8a03d2be3dbd8f110bf6c0e15173
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-09-02 20:09:07 +0600

    commit#15 Boolean Logics(and,or,not)

---
+++ 00_JS-Basics/07_JS-Boolean-Logic/scripts.js
@@ -0,0 +1,19 @@
+/*****************
+ * Boolean Logic *
+ *****************/
+
+var firstName = 'John';
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+}
+else if (age >= 13 && age < 20) { // between 13 to 20
+    console.log(firstName + ' is a teenager.');
+}
+else if (age >= 20 && age < 30){
+    console.log(firstName + ' is a young man.');
+}
+else {
+    console.log(firstName + ' is a man.');
+}

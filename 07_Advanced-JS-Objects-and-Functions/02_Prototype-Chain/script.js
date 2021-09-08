0c083a457e3293ff78541501c1e7022a78903049
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-09-08 20:10:15 +0600

    Commit#34 Prototype Chain

---
+++ 07_Advanced-JS-Objects-and-Functions/02_Prototype-Chain/script.js
@@ -0,0 +1,39 @@
+/*******************
+ * Prototype Chain *
+ *******************/
+
+// Function Constructor
+var Person = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth =yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calcAge = function(){
+    console.log(2021 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'chowdhury';
+
+var amin = new Person('amin', 1999, 'developer');
+console.log(amin);
+var parvez = new Person('parvez', 1991, 'js learner');
+var omi = new Person('omi', 2001, 'machine learning dev');
+
+amin.calcAge();
+parvez.calcAge();
+omi.calcAge();
+
+console.log(amin.lastName);
+console.log(parvez.lastName);
+console.log(omi.lastName);
+
+
+/*---------------------------------------------------------*/
+// amin.hasOwnProperty('name')
+// true
+// amin.hasOwnProperty('lastName')
+// false
+
+// amin instanceof Person
+// true

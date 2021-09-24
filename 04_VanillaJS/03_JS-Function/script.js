44f2c0532a54585e85e05764e7335336f11bf575
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-09-24 19:14:29 +0600

    commit#47 Vanilla JS Functions

---
+++ 04_VanillaJS/03_JS-Function/script.js
@@ -0,0 +1,25 @@
+function sleepTime(name, time){
+    console.log(name + " is my name, I sleep at " + time);
+}
+
+sleepTime("Ruhul", "10 PM");
+sleepTime("Nafiza", "1 AM");
+sleepTime("Parvez", "11 PM");
+
+function myFunction(a,b){
+    return a * b;
+}
+
+let x = myFunction(4, 3);
+console.log(x);
+
+let carName = "BMW";
+
+function carModel(){
+    let carName = 'Volvo';
+    console.log(carName);
+}
+
+carModel();
+
+console.log(carName);

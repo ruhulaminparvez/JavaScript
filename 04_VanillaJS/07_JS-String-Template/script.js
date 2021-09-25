cd62eef1bf120476436adf70123d01656486110e
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2021-09-25 14:39:41 +0600

    commit#50 string templates

---
+++ 04_VanillaJS/07_JS-String-Template/script.js
@@ -0,0 +1,21 @@
+/***********************
+* String Templates or  *
+* Templates String or  *
+* Template Literals or *
+* Back-Tics Syntax     *
+***********************/
+
+// Back-Tics Syntax
+let country = `Bangladesh`;
+
+// Interpolation / Substitution
+let citizen = `I love ${country}, It's a big country.`;
+
+// Expression Substitution
+let count = 15;
+let people = 3;
+
+let population = `I love ${country}, It's population is ${count + people} crore.`;
+
+console.log(citizen);
+console.log(population);
